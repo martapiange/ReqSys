a3aa07cefaa9d25e5e8cc6135ae7125a6a6447dc
MartaPianta <marta.visetti@unifr.ch>
2024-12-13 21:06:39 +0100

    login page recognises 3 users

--- campus-app/src/my-app.ts
+++ campus-app/src/my-app.ts
@@ -7,13 +7,17 @@ export class MyApp {
 
  
   private storedCredentials = {
-    email: 'giuseppe.impastato@unifr.ch',
-    password: '1234510100passi!',
+    email1: 'giuseppe.impastato@unifr.ch',
+    password1: '1234510100passi!',
+    email2: 'pino.sergi@unifr.ch',
+    password2: '7Ottobre!',
+    email3: 'lucas.mosch@unifr.ch',
+    password3: 'prova',
   };
 
   // Handle login
   login() {
-    if (this.email === this.storedCredentials.email && this.password === this.storedCredentials.password) {
+    if ((this.email === this.storedCredentials.email1 && this.password === this.storedCredentials.password1)||(this.email === this.storedCredentials.email2 && this.password === this.storedCredentials.password2)||(this.email === this.storedCredentials.email3 && this.password === this.storedCredentials.password3)) {
       alert('Login successful!');
       this.errorMessage = ''; // Clear any error message
     } else {
