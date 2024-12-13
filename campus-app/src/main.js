37be2ae4ba8b74c76decb27aef934f3d6880d7fb
MartaPianta <marta.visetti@unifr.ch>
2024-12-13 10:34:27 +0100

    working page with reduced db (only 1 student registered)

--- campus-app/src/main.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var aurelia_1 = require("aurelia");
-var my_app_1 = require("./my-app");
-aurelia_1.default
-    .app(my_app_1.MyApp) //launching of the first page
-    .start();
