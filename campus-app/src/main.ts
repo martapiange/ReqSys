b41428424e18b5ff1f0a437cd83efc80466cb00c
MartaPianta <marta.visetti@unifr.ch>
2024-12-17 14:33:11 +0100

    17.12

--- campus-app/src/main.ts
+++ campus-app/src/main.ts
@@ -1,5 +1,7 @@
 import Aurelia from 'aurelia';
 import {MyApp} from './my-app';
+import { HomePage } from './home';
+import { CoursesPage } from './courses';
 
 Aurelia
   .app(MyApp)  //launching of the first page
--- campus-app/src/my-app.ts
+++ campus-app/src/my-app.ts
@@ -1,27 +1,19 @@
+import { LoginPage } from "./log-in-page";
+import { HomePage } from "./home";
+
 
 export class MyApp {
+  static routes = [
+    {
+      path: '',
+      component: '',
+      title: 'LogIn',
+    },
+    {
+      path: 'home',
+      component: '',
+      title: 'Home',
+    },
   
-  email: string = '';
-  password: string = '';
-  errorMessage: string = '';
-
- 
-  private storedCredentials = {
-    email1: 'giuseppe.impastato@unifr.ch',
-    password1: '1234510100passi!',
-    email2: 'pino.sergi@unifr.ch',
-    password2: '7Ottobre!',
-    email3: 'lucas.mosch@unifr.ch',
-    password3: 'prova',
-  };
-
-  // Handle login
-  login() {
-    if ((this.email === this.storedCredentials.email1 && this.password === this.storedCredentials.password1)||(this.email === this.storedCredentials.email2 && this.password === this.storedCredentials.password2)||(this.email === this.storedCredentials.email3 && this.password === this.storedCredentials.password3)) {
-      alert('Login successful!');
-      this.errorMessage = ''; // Clear any error message
-    } else {
-      this.errorMessage = 'Invalid email or password!';
-    }
-  }
+  ];
 }
